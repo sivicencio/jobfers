cda487c61437ceb658cf9b9f10ed60d3122131c2
sivicencio <sivicencio@gmail.com>
2014-09-23 10:56:41 -0300

    Add view specº

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,3 +1,12 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    begin
+      DatabaseCleaner.start
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
 end
--- spec/views/offers/index.html.slim_spec.rb
+++ spec/views/offers/index.html.slim_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 describe "offers/index" do
   it 'renders a list of offers' do
+    create(:offer_with_details)
     assign(:offers, Offer.paginate(page: 1))
     render
+    expect(rendered).to have_selector('ul.offers li')
+  end
+
+  it 'renders a list of offers clickable' do
+    create(:offer_with_details)
+    assign(:offers, Offer.paginate(page: 1))
+    render
+    expect(rendered).to have_selector('ul.offers li a')
   end
 end
