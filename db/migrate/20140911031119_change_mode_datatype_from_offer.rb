6db1a19bbfc8d12f3613763b667551bf2e365498
sivicencio <sivicencio@gmail.com>
2014-09-23 00:38:34 -0300

    Fix for Offer mode migration

--- db/migrate/20140911031119_change_mode_datatype_from_offer.rb
+++ db/migrate/20140911031119_change_mode_datatype_from_offer.rb
@@ -1,7 +1,7 @@
 class ChangeModeDatatypeFromOffer < ActiveRecord::Migration
   def change
     change_table :offers do |t|
-      t.change :mode, :integer
+      t.change :mode, :integer, 'integer USING CAST(mode AS integer)'
     end
   end
 end
